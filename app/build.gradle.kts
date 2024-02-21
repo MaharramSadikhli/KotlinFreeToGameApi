86ab93e59ab42026a9bc59eecb8833c19216cbd2
Muarem <s.muarem10@gmail.com>
2024-02-21 06:20:53 +0400

    added kotlin kapt and ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.jetbrains.kotlin.kapt)
 }
 
 android {
