0d5c62022cd27711bb65d616640586fba50c3bd5
Muarem <s.muarem10@gmail.com>
2024-02-22 11:28:11 +0400

    created ImageView extension func

---
+++ app/src/main/java/com/example/kotlinfreegamelist/util/Utils.kt
@@ -0,0 +1,27 @@
+package com.example.kotlinfreegamelist.util
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+
+fun ImageView.loadImage(url: String, progressDrawable: CircularProgressDrawable){
+    val options = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(com.example.kotlinfreegamelist.R.mipmap.ic_launcher_round)
+
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(url)
+        .into(this)
+}
+
+fun placeHolderProgressBar (context: Context): CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 8f
+        centerRadius = 40f
+        start()
+    }
+}
