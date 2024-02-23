caf4b701fd4671938acff49e3d6707c91e17b5a4
Muarem <s.muarem10@gmail.com>
2024-02-23 12:38:15 +0400

    created loadImageXML func for dataBinding

--- app/src/main/java/com/example/kotlinfreegamelist/util/Utils.kt
+++ app/src/main/java/com/example/kotlinfreegamelist/util/Utils.kt
@@ -2,6 +2,7 @@ package com.example.kotlinfreegamelist.util
 
 import android.content.Context
 import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -24,4 +25,10 @@ fun placeHolderProgressBar (context: Context): CircularProgressDrawable {
         centerRadius = 40f
         start()
     }
+}
+
+
+@BindingAdapter(value = ["android:imageUrl"])
+fun loadImageXML(view: ImageView, url: String) {
+    view.loadImage(url, placeHolderProgressBar(view.context))
 }
